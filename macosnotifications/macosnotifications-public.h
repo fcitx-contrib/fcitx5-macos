463468c49b3dd273074e337a14a9b2bbc16c1de9
Qijia Liu <liumeo@pku.edu.cn>
2024-01-14 23:14:09 -0500

    format; vscode extensions (#20)

--- macosnotifications/macosnotifications-public.h
+++ macosnotifications/macosnotifications-public.h
@@ -2,25 +2,28 @@
 
 #include <cstdint>
 
-// Refer to https://specifications.freedesktop.org/notification-spec/notification-spec-latest.html
+// Refer to
+// https://specifications.freedesktop.org/notification-spec/notification-spec-latest.html
 enum NotificationClosedReason {
-     /// The notification expired.
-     NOTIFICATION_CLOSED_REASON_EXPIRY = 1,
+    /// The notification expired.
+    NOTIFICATION_CLOSED_REASON_EXPIRY = 1,
 
-     /// The notification was dismissed by the user.
-     NOTIFICATION_CLOSED_REASON_DISMISSED = 2,
+    /// The notification was dismissed by the user.
+    NOTIFICATION_CLOSED_REASON_DISMISSED = 2,
 
-     /// The notification was closed by a call to CloseNotification.
-     NOTIFICATION_CLOSED_REASON_CLOSED = 3,
+    /// The notification was closed by a call to CloseNotification.
+    NOTIFICATION_CLOSED_REASON_CLOSED = 3,
 
-     /// Undefined/reserved reasons.
-     NOTIFICATION_CLOSED_REASON_UNDEFINED = 4,
+    /// Undefined/reserved reasons.
+    NOTIFICATION_CLOSED_REASON_UNDEFINED = 4,
 };
 
 namespace fcitx {
 
-void handleActionResult(const char *notificationId, const char *actionId) noexcept;
+void handleActionResult(const char *notificationId,
+                        const char *actionId) noexcept;
 
-void destroyNotificationItem(const char *notificationId, uint32_t closed_reason) noexcept;
+void destroyNotificationItem(const char *notificationId,
+                             uint32_t closed_reason) noexcept;
 
-}
+} // namespace fcitx
