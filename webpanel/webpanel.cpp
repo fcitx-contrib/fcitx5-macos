fd252e665b167f4151266c271f239c16d5b3f392
ksqsf <i@ksqsf.moe>
2024-02-26 13:46:09 +0800

    prevent crash

--- webpanel/webpanel.cpp
+++ webpanel/webpanel.cpp
@@ -102,9 +102,10 @@ void WebPanel::update(UserInterfaceComponent component,
         window_->set_candidates(candidates, labels, highlighted);
         window_->set_layout(layout);
         updatePanelShowFlags(!candidates.empty(), PanelShowFlag::HasCandidates);
-        dynamic_cast<MacosInputContext *>(inputContext)
-            ->forcePreedit((panelShow_ & PanelShowFlag::HasPreedit) |
-                           (panelShow_ & PanelShowFlag::HasCandidates));
+        if (auto macosIC = dynamic_cast<MacosInputContext *>(inputContext)) {
+            macosIC->forcePreedit((panelShow_ & PanelShowFlag::HasPreedit) |
+                                  (panelShow_ & PanelShowFlag::HasCandidates));
+        }
         showAsync(panelShow_);
         break;
     }
