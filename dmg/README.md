8f2a36ab7ff42fcdf08132c81fffa77be1609a17
Qijia Liu <liumeo@pku.edu.cn>
2024-03-15 21:06:31 -0400

    drop dmg and use tar.bz2 for beta release (#85)

--- dmg/README.md
+++
@@ -1,9 +0,0 @@
-This directory contains files necessary to generate Fcitx5.dmg.
-
-[fcitx.icns](./fcitx.icns) is generated from the official [logo](../fcitx5/data/icon/scalable/apps/org.fcitx.Fcitx5.svg).
-
-[backgroud.tiff](./background.tiff) is generated with
-```sh
-tiffutil -cathidpicheck background.png background@2x.png -out background.tiff
-```
-where the 2 PNGs are exported from draw.io with 100% and 200% scale.
--- fix-cross-build.sh
+++ fix-cross-build.sh
@@ -1,3 +1,4 @@
+# Currently not used, but will be useful once GitHub drops macOS x86 runners
 set -e
 if [[ -z "$CI" ]]; then
   exit 1
@@ -7,9 +8,8 @@ if [[ $1 == x86_64 ]]; then
   exit 1
 fi
 # HACK: temporarily replace comp-spell-dict from last successful build of native fcitx5
-wget https://github.com/fcitx-contrib/fcitx5-macos/releases/download/latest/Fcitx5-x86_64.dmg
-hdiutil attach Fcitx5-x86_64.dmg
-sudo cp -r /Volumes/Fcitx5/{Fcitx5.app,"Input Methods"}
+wget https://github.com/fcitx-contrib/fcitx5-macos/releases/download/latest/Fcitx5-x86_64.tar.bz2
+sudo tar xjvf Fcitx5-x86_64.tar.bz2 -C "/Library/Input Methods"
 mv build/fcitx5/src/modules/spell/comp-spell-dict{,.bak}
 cp "/Library/Input Methods/Fcitx5.app/Contents/lib/fcitx5/libexec/comp-spell-dict" build/fcitx5/src/modules/spell/comp-spell-dict
 cmake --build build
