8924f6d5ad8d93f733d6fa12a1e6fbf18a826777
Qijia Liu <liumeo@pku.edu.cn>
2024-03-19 21:40:28 -0400

    focus config window on open

--- src/config/about.swift
+++ src/config/about.swift
@@ -8,7 +8,7 @@ struct AboutView: View {
   }
 }
 
-class FcitxAbout: NSWindowController {
+class FcitxAbout: ConfigWindowController {
   convenience init() {
     let window = NSWindow(
       contentRect: NSRect(x: 0, y: 0, width: 480, height: 300),
