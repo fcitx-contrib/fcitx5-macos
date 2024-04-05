593d3a473892ed1f51b2d37955b5786f3a3b1ebd
Qijia Liu <liumeo@pku.edu.cn>
2024-04-04 22:42:11 -0400

    preserve icon on update

--- assets/update.sh
+++ assets/update.sh
@@ -7,9 +7,18 @@ INSTALL_DIR="/Library/Input Methods"
 APP_DIR="$INSTALL_DIR/Fcitx5.app"
 RESOURCES_DIR="$APP_DIR/Contents/Resources"
 
+ICON_FILE="fcitx.icns"
+ICON_PATH="$RESOURCES_DIR/$ICON_FILE"
+ICON_BAKUP="/tmp/$ICON_FILE"
+
+# Backup maybe user-defined icon
+[[ -f "$ICON_PATH" ]] && mv "$ICON_PATH" "$ICON_BAKUP"
 rm -rf "$APP_DIR/Contents/*"
+
 tar xjvf "$tar_ball" -C "$INSTALL_DIR"
 rm -f "$tar_ball"
+[[ -f "$ICON_BAKUP" ]] && mv "$ICON_BAKUP" "$ICON_PATH"
+
 xattr -dr com.apple.quarantine "$APP_DIR"
 codesign --force --sign - --deep "$APP_DIR"
 
