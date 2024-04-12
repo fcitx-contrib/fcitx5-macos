ab1e3b6ed9b7e6734176dbdc84e3940847eaeb31
Qijia Liu <liumeo@pku.edu.cn>
2024-04-12 00:05:32 -0400

    write swift log to stderr

--- logging/logging.swift
+++ logging/logging.swift
@@ -3,17 +3,29 @@ import OSLog
 let logger = Logger(subsystem: "org.fcitx.inputmethod.Fcitx5", category: "FcitxLog")
 
 public func FCITX_DEBUG(_ message: String) {
+  if isDebug {
     logger.debug("\(message, privacy: .public)")
+    fputs(message + "\n", stderr)
+  }
 }
 
 public func FCITX_INFO(_ message: String) {
+  if isDebug {
     logger.info("\(message, privacy: .public)")
+  }
+  fputs(message + "\n", stderr)
 }
 
 public func FCITX_WARN(_ message: String) {
+  if isDebug {
     logger.error("\(message, privacy: .public)")
+  }
+  fputs(message + "\n", stderr)
 }
 
 public func FCITX_ERROR(_ message: String) {
+  if isDebug {
     logger.fault("\(message, privacy: .public)")
+  }
+  fputs(message + "\n", stderr)
 }
