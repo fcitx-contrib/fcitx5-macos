faecd5cbb74b97c0e738a949df3130a329ac6237
Qijia Liu <liumeo@pku.edu.cn>
2024-05-20 08:50:31 -0400

    import data from hamster (#128)

--- src/config/advanced.swift
+++ src/config/advanced.swift
@@ -22,17 +22,42 @@ class AdvancedController: ConfigWindowController {
 
 private func extractZip(_ file: URL) -> Bool {
   // unzip is unfriendly with Chinese file names
-  if !exec("/usr/bin/ditto", ["-xk", file.localPath(), extractPath]) {
-    return false
-  }
-  return extractDir.appendingPathComponent("metadata.json").exists()
+  return exec("/usr/bin/ditto", ["-xk", file.localPath(), extractPath])
 }
 
 struct AdvancedView: View {
-  let openPanel = NSOpenPanel()
+  @State private var openPanel = NSOpenPanel()
   @AppStorage("ImportDataSelectedDirectory") var importDataSelectedDirectory: String?
   @State private var showImportF5a = false
   @State private var showImportSquirrel = false
+  @State private var showImportHamster = false
+  @State private var showAlert = false
+
+  private func importZip(_ binding: Binding<Bool>, _ validator: @escaping () -> Bool) {
+    // Keep a single openPanel to avoid confusion.
+    if openPanel.isVisible {
+      openPanel.cancel(nil)
+      openPanel = NSOpenPanel()
+    }
+    openPanel.allowsMultipleSelection = false
+    openPanel.canChooseDirectories = false
+    openPanel.allowedContentTypes = [UTType.init(filenameExtension: "zip")!]
+    openPanel.directoryURL = URL(
+      fileURLWithPath: importDataSelectedDirectory
+        ?? homeDir.appendingPathComponent("Downloads").localPath())
+    openPanel.begin { response in
+      if response == .OK {
+        removeFile(extractDir)
+        mkdirP(extractPath)
+        if let file = openPanel.urls.first, extractZip(file), validator() {
+          binding.wrappedValue = true
+        } else {
+          showAlert = true
+        }
+      }
+      importDataSelectedDirectory = openPanel.directoryURL?.localPath()
+    }
+  }
 
   var body: some View {
     VStack {
@@ -50,27 +75,41 @@ struct AdvancedView: View {
       }.disabled(!FileManager.default.fileExists(atPath: squirrelDir.localPath()))
 
       Button {
-        openPanel.allowsMultipleSelection = false
-        openPanel.canChooseDirectories = false
-        openPanel.allowedContentTypes = [UTType.init(filenameExtension: "zip")!]
-        openPanel.directoryURL = URL(
-          fileURLWithPath: importDataSelectedDirectory
-            ?? homeDir.appendingPathComponent("Downloads").localPath())
-        openPanel.begin { response in
-          if response == .OK {
-            removeFile(extractDir)
-            mkdirP(extractPath)
-            if let file = openPanel.urls.first, extractZip(file) {
-              showImportF5a = true
-            }
-          }
-          importDataSelectedDirectory = openPanel.directoryURL?.localPath()
-        }
+        importZip(
+          $showImportF5a,
+          {
+            extractDir.appendingPathComponent("metadata.json").exists()
+          })
       } label: {
-        Text("Fcitx5 Android")
+        Text("Fcitx5 Android").tooltip("fcitx5-android_YYYY-MM-DD*.zip")
       }.sheet(isPresented: $showImportF5a) {
         ImportDataView(f5aItems)
       }
+
+      Button {
+        importZip(
+          $showImportHamster,
+          {
+            hamsterRimeDir.exists()
+          })
+      } label: {
+        Text("Hamster").tooltip("YYYYMMDD-*.zip")
+      }.sheet(isPresented: $showImportHamster) {
+        ImportDataView(hamsterItems)
+      }
+    }.alert(
+      Text("Error"),
+      isPresented: $showAlert,
+      presenting: ()
+    ) { _ in
+      Button {
+        showAlert = false
+      } label: {
+        Text("OK")
+      }
+      .buttonStyle(.borderedProminent)
+    } message: { _ in
+      Text("Invalid zip.")
     }.padding()
   }
 }
