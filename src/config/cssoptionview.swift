031751af1d58f2bd4f4082ba4258daf199d50de3
ksqsf <i@ksqsf.moe>
2024-07-17 19:55:24 +0200

    add an erase button

--- src/config/cssoptionview.swift
+++ src/config/cssoptionview.swift
@@ -11,22 +11,29 @@ struct CssOptionView: OptionView {
   @State private var openPanel = NSOpenPanel()
 
   var body: some View {
-    Button {
-      selectFile(
-        openPanel, cssDir, [UTType.init(filenameExtension: "css")!],
-        { fileName in
-          if fileName.isEmpty {
-            model.value = ""
-          } else {
-            model.value = fcitxPrefix + fileName
-          }
-        })
-    } label: {
-      if !model.value.hasPrefix(fcitxPrefix) {
-        Text("Select/Import CSS")
-      } else {
-        Text(model.value.dropFirst(fcitxPrefix.count))
+    HStack {
+      Button {
+        selectFile(
+          openPanel, cssDir, [UTType.init(filenameExtension: "css")!],
+          { fileName in
+            if !fileName.isEmpty {
+              model.value = fcitxPrefix + fileName
+            }
+          })
+      } label: {
+        if !model.value.hasPrefix(fcitxPrefix) {
+          Text("Select/Import CSS")
+        } else {
+          Text(model.value.dropFirst(fcitxPrefix.count))
+        }
       }
-    }
+      if model.value.hasPrefix(fcitxPrefix) {
+        Button {
+          model.value = ""
+        } label: {
+          Image(systemName: "xmark.circle.fill")
+        }.buttonStyle(BorderlessButtonStyle())
+      }
+    }.padding(.horizontal)
   }
 }
