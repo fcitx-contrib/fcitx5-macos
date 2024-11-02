bfa780301803e770b8a6ad9b2e510393a7847c35
ksqsf <i@ksqsf.moe>
2024-11-02 23:18:44 +0100

    define weak symbols in webpanel
    possibly for f5j

---
+++ webpanel/tunnel.cpp
@@ -0,0 +1,4 @@
+// Weak definitions for tunnel variables used in webpanel.
+__attribute__((weak)) bool f5m_is_linear_layout = false;
+__attribute__((weak)) bool f5m_is_vertical_rl = false;
+__attribute__((weak)) bool f5m_is_vertical_lr = false;
