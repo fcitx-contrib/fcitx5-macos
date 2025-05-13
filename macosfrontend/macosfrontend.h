e2bd6f07b8fd82f2f7ff72c4d7ad5b1ac460f8e9
Qijia Liu <liumeo@pku.edu.cn>
2025-05-12 23:38:10 -0400

    Revert "Passwords.app uses keyboard-us (#221)"
    This reverts commit 815e06c9599eca42a314037ad1fec8c98921e230.

--- macosfrontend/macosfrontend.h
+++ macosfrontend/macosfrontend.h
@@ -23,10 +23,6 @@
 #define TERMINAL_USE_EN                                                        \
     R"JSON({"appPath": "/System/Applications/Utilities/Terminal.app", "appId": "com.apple.Terminal", "imName": "keyboard-us"})JSON"
 
-// User deletes ABC to enable Fcitx5 on password input.
-#define PASSWORDS_USE_EN                                                       \
-    R"JSON({"appPath": "/System/Applications/Passwords.app", "appId": "com.apple.Passwords", "imName": "keyboard-us"})JSON"
-
 namespace fcitx {
 
 class MacosInputContext;
@@ -42,10 +38,8 @@ struct AppIMAnnotation {
 FCITX_CONFIGURATION(
     MacosFrontendConfig,
     OptionWithAnnotation<std::vector<std::string>, AppIMAnnotation>
-        appDefaultIM{this,
-                     "AppDefaultIM",
-                     _("App default IM"),
-                     {TERMINAL_USE_EN, PASSWORDS_USE_EN}};
+        appDefaultIM{
+            this, "AppDefaultIM", _("App default IM"), {TERMINAL_USE_EN}};
     Option<bool> simulateKeyRelease{this, "SimulateKeyRelease",
                                     _("Simulate key release")};
     Option<int, IntConstrain> simulateKeyReleaseDelay{
