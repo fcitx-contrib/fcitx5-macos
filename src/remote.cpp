c3f705ba9270a585acce6301bfa9256e4a8f49c4
Qijia Liu <liumeo@pku.edu.cn>
2025-12-27 13:37:46 -0500

    fcitx5-remote; beast i18n

---
+++ src/remote.cpp
@@ -0,0 +1,38 @@
+#include "fcitx.h"
+
+using json = nlohmann::json;
+
+std::pair<bool, std::string> remoteHandler(const std::string_view command,
+                                           const char *body) {
+    return with_fcitx([&](Fcitx &fcitx) -> std::pair<bool, std::string> {
+        if (command == "c") {
+            fcitx.instance()->deactivate();
+            return {true, ""};
+        }
+        if (command == "o") {
+            fcitx.instance()->activate();
+            return {true, ""};
+        }
+        if (command == "t" || command == "T") {
+            toggleInputMethod();
+            return {true, ""};
+        }
+        if (command == "n") {
+            return {true, imGetCurrentIMName() + "\n"};
+        }
+        if (command == "s") {
+            try {
+                auto j = json::parse(body);
+                if (!j.is_array() || j.size() != 1 || !j[0].is_string()) {
+                    return {false, "Invalid array\n"};
+                }
+                auto im = j[0].get<std::string>();
+                imSetCurrentIM(im.c_str());
+                return {true, ""};
+            } catch (const std::exception &e) {
+                return {false, "Invalid JSON\n"};
+            }
+        }
+        return {false, "Unknown command\n"};
+    });
+}
